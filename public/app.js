253c3f61a3e3d7fc75ab9088161ff45e544a98cb
amit chanchal <amit.chanchal@hotmail.com>
2018-04-10 07:56:43 +0530

    base code

---
+++ public/app.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+  el: '#app',
+  data:{
+    appName: 'emailFormatter',
+    rName: null,
+    sName: null,
+    orgName: null,
+    emailType: null,
+    showToggle: false,
+  },
+  computed:{
+    typeItems(){
+
+    },
+
+  },
+  methods:{
+    submit(){
+
+    },
+    back(){
+
+    }
+  }
+
+});
