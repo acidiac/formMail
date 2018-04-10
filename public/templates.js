f1f3e391de006a638d1936821b4f3d49dbfad857
amit chanchal <amit.chanchal@hotmail.com>
2018-04-10 07:57:58 +0530

    base code

---
+++ public/templates.js
@@ -0,0 +1,34 @@
+var templates = [
+  {
+    name:"Welcome",
+    purpose: "To welcome new email subscribers to the family and establish a good relationship.",
+    tips:[
+      {
+        title:"Write in a conversational tone.", 
+        content:"A welcome email is like a virtual handshake that accepts a new member into your group. It should be inviting and warm. Show the personal side of your business. Take a look at the email below. It specifically welcomes new subscribers into a community, and the tone is friendly and casual, but still professional."
+      },
+      {
+        title:"Consider offering a reward.", 
+        content:"In celebration of a new customer, you could offer a discount or some sort of perk. You could offer 10 percent off the next purchase like Society6 did in the example above, or offer another perk like free shipping."
+      },
+      {
+        title:"Remind new users about the benefits", 
+        content:"Thank your new customers for signing up and reinforce their decision to join. Tell readers what they’ll get out of this new partnership. The email below does exactly that."
+      },
+    ],
+    templates:[
+
+    ],
+    additionalInput:[],
+  },
+  {
+    name:'Educational',
+    purpose: 'To provide customers with industry knowledge that’s connected to your business or product. It helps build relationships and trust between your business and your customers.',
+    tips:[
+
+    ],
+    templates:[],
+    additionalInput:[]
+  }
+
+]
